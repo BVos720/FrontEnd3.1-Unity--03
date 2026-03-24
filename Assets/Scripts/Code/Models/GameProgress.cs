4d3e76aa249831eab270e5545b5610260ccb46db
Branco Vos <b.vos5@student.avans.nl>
2026-03-24 14:04:05 +0100

    Overzicht werkend (Denk ik)

--- Assets/Scripts/Code/Models/GameProgress.cs
+++ Assets/Scripts/Code/Models/GameProgress.cs
@@ -6,6 +6,10 @@ namespace MySecureBackend.WebApi.Models
 {
     public class GameProgress
     {
+        public Guid GameProgressID { get; set; }
+
+        public Guid BehandelingID { get; set; }
+
         public float LevelProgress { get; set; }
 
         public int Points { get; set; }
