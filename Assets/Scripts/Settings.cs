776f42c83fef3b307684181912db5752ca0512ec
Branco Vos <b.vos5@student.avans.nl>
2026-03-25 12:56:05 +0100

    Settings worden in back end afgehandeld

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 using UnityEngine;
 using TMPro;
-using Assets.Scripts;
 using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
+using MySecureBackend.WebApi.Models;
 
 namespace Assets.Scripts
 {
@@ -15,20 +14,43 @@ namespace Assets.Scripts
         [SerializeField] private UnityEngine.UI.RawImage willie;
         [SerializeField] private TMP_Dropdown colorBlindDropdown;
         [SerializeField] private UnityEngine.UI.RawImage colorBlindImage;
+        public SettingsApiClient settingsApiClient;
 
-        private void Start()
+        private Guid? _settingsID = null;
+
+        private async void Start()
         {
             if (characterDropdown != null)
                 characterDropdown.onValueChanged.AddListener(OnCharacterChanged);
 
-            // Zet dropdown op opgeslagen keuze
+            if (colorBlindDropdown != null)
+                colorBlindDropdown.onValueChanged.AddListener(OnColorBlindChanged);
+
+            // Probeer settings te laden vanuit de backend
+            if (settingsApiClient != null)
+            {
+                IWebRequestReponse response = await settingsApiClient.GetSettings();
+                switch (response)
+                {
+                    case WebRequestData<string> dataResponse:
+                        SettingsData loaded = JsonConvert.DeserializeObject<SettingsData>(dataResponse.Data);
+                        if (loaded != null)
+                        {
+                            _settingsID = loaded.SettingsID;
+                            PlayerPrefs.SetInt("SelectedCharacter", loaded.Character);
+                            PlayerPrefs.SetInt("ColorBlindSetting", loaded.ColorTheme);
+                            PlayerPrefs.Save();
+                        }
+                        break;
+                    case WebRequestError errorResponse:
+                        Debug.LogWarning("Kon settings niet laden: " + errorResponse.ErrorMessage);
+                        break;
+                }
+            }
+
             int selected = PlayerPrefs.GetInt("SelectedCharacter", 0);
             characterDropdown.value = selected;
             OnCharacterChanged(selected);
-
-            // Colorblind dropdown
-            if (colorBlindDropdown != null)
-                colorBlindDropdown.onValueChanged.AddListener(OnColorBlindChanged);
 
             int colorBlindSelected = PlayerPrefs.GetInt("ColorBlindSetting", 0);
             colorBlindDropdown.value = colorBlindSelected;
@@ -40,26 +62,65 @@ namespace Assets.Scripts
             if (ballo == null || willie == null) return;
             PlayerPrefs.SetInt("SelectedCharacter", index);
             PlayerPrefs.Save();
+
             if (index == 0) // Ballo
             {
                 SetAlpha(ballo, 1f);
-                SetAlpha(willie, 150f/255f);
+                SetAlpha(willie, 150f / 255f);
             }
             else if (index == 1) // Willie
             {
-                SetAlpha(ballo, 150f/255f);
+                SetAlpha(ballo, 150f / 255f);
                 SetAlpha(willie, 1f);
             }
             else // Andere
             {
-                SetAlpha(ballo, 150f/255f);
-                SetAlpha(willie, 150f/255f);
+                SetAlpha(ballo, 150f / 255f);
+                SetAlpha(willie, 150f / 255f);
             }
 
-            // Update alle Karakter componenten in de scene
             foreach (var karakter in FindObjectsOfType<Karakter>())
-            {
                 karakter.SetActiveKarakter();
+
+            _ = SaveSettings();
+        }
+
+        private void OnColorBlindChanged(int index)
+        {
+            PlayerPrefs.SetInt("ColorBlindSetting", index);
+            PlayerPrefs.Save();
+
+            if (colorBlindImage != null)
+                colorBlindImage.gameObject.SetActive(index == 1);
+
+            _ = SaveSettings();
+        }
+
+        private async Awaitable SaveSettings()
+        {
+            if (settingsApiClient == null || !_settingsID.HasValue) return;
+
+            string kindIDString = PlayerPrefs.GetString("kindID", "");
+            if (!Guid.TryParse(kindIDString, out Guid kindID)) return;
+
+            SettingsData settingsData = new SettingsData(characterDropdown.value, colorBlindDropdown.value)
+            {
+                SettingsID = _settingsID.Value,
+                KindID = kindID
+            };
+
+            IWebRequestReponse webRequestResponse = await settingsApiClient.UpdateItem(_settingsID.Value, settingsData);
+
+            switch (webRequestResponse)
+            {
+                case WebRequestData<string> dataResponse:
+                    Debug.Log("Update settings success");
+                    break;
+                case WebRequestError errorResponse:
+                    Debug.Log("Update settings error: " + errorResponse.ErrorMessage);
+                    break;
+                default:
+                    throw new NotImplementedException("No implementation for webRequestResponse of class: " + webRequestResponse.GetType());
             }
         }
 
@@ -74,34 +135,9 @@ namespace Assets.Scripts
         {
             string previousScene = PlayerPrefs.GetString("PreviousScene", "");
             if (!string.IsNullOrEmpty(previousScene))
-            {
                 SceneManager.LoadScene(previousScene);
-            }
             else
-            {
-                // Fallback: laad eerste scene als er geen vorige bekend is
                 SceneManager.LoadScene(0);
-            }
-        }
-
-        private void OnColorBlindChanged(int index)
-        {
-            PlayerPrefs.SetInt("ColorBlindSetting", index);
-            PlayerPrefs.Save();
-            if (colorBlindImage == null) return;
-
-            if (index == 0)
-            {
-                colorBlindImage.gameObject.SetActive(false);
-            }
-            else if (index == 1)
-            {
-                colorBlindImage.gameObject.SetActive(true);
-            }
-            else
-            {
-                colorBlindImage.gameObject.SetActive(false);
-            }
         }
     }
 }
